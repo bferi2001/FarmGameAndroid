2c7f4a48e041f6492f15de460e573fc44d771051
bferi2001 <barnafeco@gmail.com>
2024-10-31 11:46:34 +0100

    Entities, DBContext added + migration

---
+++ FarmGameBackend/FarmGameBackend/Entity/Barn.cs
@@ -0,0 +1,13 @@
+﻿namespace FarmGameBackend.Entity
+{
+    public class Barn
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int TypeId { get; set; }
+        public DateTimeOffset ProductionStartTime { get; set; }
+        public DateTimeOffset ProductionEndTime { get; set; }
+        public DateTimeOffset FeedingTime { get; set; }
+        public DateTimeOffset CleaningTime { get; set; }
+    }
+}
---
+++ FarmGameBackend/FarmGameBackend/Entity/User.cs
@@ -0,0 +1,10 @@
+﻿namespace FarmGameBackend.Entity
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public int UserXP { get; set; }
+        public int UserMoney { get; set; }
+    }
+}
