c042df1c49d5c3a1a7de4250d2ea70a42b239600
bferi2001 <barnafeco@gmail.com>
2024-11-09 12:59:56 +0100

    Crops watering, weeding, fertilising endpoints added

--- FarmGameBackend/FarmGameBackend/Controllers/FarmController.cs
+++ FarmGameBackend/FarmGameBackend/Controllers/FarmController.cs
@@ -1,5 +1,6 @@
 ﻿using FarmGameBackend.DbContexts;
 using FarmGameBackend.Entity;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,7 +10,7 @@ namespace FarmGameBackend.Controllers
     public class FarmController : Controller
     {
         private readonly FarmApplicationContext _context;
-        
+
         public FarmController(FarmApplicationContext context)
         {
             _context = context;
@@ -20,27 +21,125 @@ namespace FarmGameBackend.Controllers
 
         }
 
+        [HttpGet("farm/plant/unlocked")]
+        public async Task<ActionResult<IEnumerable<string>>> GetUnlockedCropsAsync(int position)
+        {
+
+        }
+
         [HttpPost("farm/plant/{position}/{typeName}")]
         public async Task<ActionResult<PlantedPlant>> PostPlantedPlant(int position, string typeName)
         {
-            DateTimeOffset currentTime = DateTimeOffset.Now;
-            Product? productType = getProductByName(typeName);
-            if(productType == null)
+            if (GetPlantByPosition(position) != null)
+            {
+                return Conflict("The field is not empty");
+            }
+            Product? productType = GetUnlockedProductByName(typeName);
+            if (productType == null)
             {
                 return NotFound();
             }
-            //ToDo Check if crop is unlocked
+
+            DateTimeOffset currentTime = DateTimeOffset.Now;
             int growTime = productType.ProductionTimeAsSeconds;
-            var newPlant = new PlantedPlant(typeName, position, growTime);
+            PlantedPlant newPlant = CreatePlantedPlant(typeName, position, growTime);
 
             _context.PlantedPlants.Add(newPlant);
             await _context.SaveChangesAsync();
 
             return CreatedAtAction("GetPlantedPlant", new { id = newPlant.Id }, newPlant);
         }
-        public Product? getProductByName(string name)
+
+        [HttpPut("farm/watering/{position}")]
+        public async Task<IActionResult> PutWatering(int position)
         {
-            if (!DoesProductExists(name))
+            PlantedPlant plantAtPosition = GetPlantByPosition(position);
+            if (plantAtPosition == null)
+            {
+                return NotFound();
+            }
+            if (plantAtPosition.WateringTime == null)
+            {
+                return BadRequest("This plant got already watered.");
+            }
+            DateTimeOffset currentTime = DateTimeOffset.Now;
+            if (currentTime >= plantAtPosition.WateringTime)
+            {
+                plantAtPosition.WateringTime = null;
+                //ToDo
+            }
+            return await UpdatePlantedPlantDatabase(plantAtPosition);
+        }
+        [HttpPut("farm/weeding/{position}")]
+        public async Task<IActionResult> PutWeeding(int position)
+        {
+            PlantedPlant plantAtPosition = GetPlantByPosition(position);
+            if (plantAtPosition == null)
+            {
+                return NotFound();
+            }
+            if (plantAtPosition.WeedingTime == null)
+            {
+                return BadRequest("This plant got already watered.");
+            }
+            DateTimeOffset currentTime = DateTimeOffset.Now;
+            if (currentTime >= plantAtPosition.WeedingTime)
+            {
+                plantAtPosition.WeedingTime = null;
+                //ToDo
+            }
+            return await UpdatePlantedPlantDatabase(plantAtPosition);
+        }
+
+        [HttpPut("farm/fertilising/{position}")]
+        public async Task<IActionResult> PutFertilising(int position)
+        {
+            PlantedPlant plantAtPosition = GetPlantByPosition(position);
+            if (plantAtPosition == null)
+            {
+                return NotFound();
+            }
+            if (plantAtPosition.FertilisingTime == null)
+            {
+                return BadRequest("This plant got already fertilised.");
+            }
+            DateTimeOffset currentTime = DateTimeOffset.Now;
+            if (currentTime >= plantAtPosition.FertilisingTime)
+            {
+                plantAtPosition.FertilisingTime = null;
+                //ToDo Other actions?
+            }
+            return await UpdatePlantedPlantDatabase(plantAtPosition);
+        }
+
+        [HttpDelete("farm/collecting/{id}")]
+        public async Task<IActionResult> DeletePlantedPlant(int id)
+        {
+
+        }
+        public async Task<IActionResult> UpdatePlantedPlantDatabase(PlantedPlant updatedPlant)
+        {
+            _context.Entry(updatedPlant).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(updatedPlant.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+        public Product? GetUnlockedProductByName(string name)
+        {
+            if (!DoesCroptypeExistsAndUnlocked(name))
             {
                 return null;
             }
@@ -53,40 +152,42 @@ namespace FarmGameBackend.Controllers
             return _context.Products.Any(product => product.Name == _productName);
         }
 
-        public List<string> GetUnlockedCrops(int _userXP)
+        public List<string> GetUnlockedCrops()
         {
-            return _context.Products.Where(product => product.UnlockXP>= _userXP && product.IsCrop)
+            int userXP = _context.GetCurrentUser().UserXP;
+            return _context.Products.Where(product => product.UnlockXP >= userXP && product.IsCrop)
                                     .Select(product => product.Name)
                                     .ToList();
         }
 
-        public Boolean IsCropUnlocked(int _userXP, string _cropName)
+        public Boolean DoesCroptypeExistsAndUnlocked(string _cropName)
         {
-            return GetUnlockedCrops(_userXP).Any(cropName => cropName == _cropName);
+            return GetUnlockedCrops().Any(cropName => cropName == _cropName);
+        }
+        public PlantedPlant GetPlantByPosition(int position)
+        {
+            return _context.PlantedPlants.Where(plant => plant.Position == position && plant.UserName == _context.GetCurrentUser().Email).First();
         }
 
-        [HttpPut("farm/watering/{position}")]
-        public async Task<IActionResult> PutWatering(int position)
+        public PlantedPlant CreatePlantedPlant(string _cropsTypeName, int _position, int _growTime)
         {
-
+            Random r = new Random();
+            DateTimeOffset currentTime = DateTimeOffset.Now;
+            return new PlantedPlant
+            {
+                UserName = _context.GetCurrentUser().Email,
+                CropsTypeName = _cropsTypeName,
+                Position = _position,
+                PlantTime = currentTime,
+                HarvestTime = currentTime.AddSeconds(_growTime),
+                WateringTime = currentTime.AddSeconds(r.Next(_growTime)),
+                WeedingTime = currentTime.AddSeconds(r.Next(_growTime)),
+                FertilisingTime = currentTime.AddSeconds(r.Next(_growTime))
+            };
         }
-        [HttpPut("farm/weeding/{position}")]
-        public async Task<IActionResult> PutWeeding(int position)
+        private bool ProductExists(int id)
         {
-
+            return _context.Products.Any(e => e.Id == id);
         }
-
-        [HttpPut("farm/fertilising/{position}")]
-        public async Task<IActionResult> PutFertilising(int position)
-        {
-
-        }
-
-        [HttpDelete("farm/collecting/{id}")]
-        public async Task<IActionResult> DeletePlantedPlant(int id)
-        {
-            
-        }
-
     }
 }
