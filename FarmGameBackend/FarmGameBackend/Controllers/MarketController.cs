544435404110c324ab9cd1ce03f13f2daea4064b
bferi2001 <barnafeco@gmail.com>
2024-11-21 16:52:23 +0100

    MarketController skeleton

---
+++ FarmGameBackend/FarmGameBackend/Controllers/MarketController.cs
@@ -0,0 +1,56 @@
+﻿using FarmGameBackend.DbContexts;
+using FarmGameBackend.Entity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using static FarmGameBackend.Helper.ProductHelper;
+
+namespace FarmGameBackend.Controllers
+{
+    [Route("api/farm")]
+    public class MarketController : Controller
+    {
+        private readonly FarmApplicationContext _context;
+        private readonly User _currentUser;
+        public MarketController(FarmApplicationContext context)
+        {
+            _context = context;
+            //string? userEmail = HttpContext.Items["Email"]?.ToString();
+            //_currentUser = _context.GetCurrentUser(userEmail!);
+            _currentUser = _context.GetCurrentUser("testemail@gmail.com");
+        }
+
+
+        [HttpGet("market")]
+        public async Task<ActionResult<IEnumerable<MarketUserProduct>>> GetUserProductsForMarketAsync()
+        {
+            //ToDo
+        }
+
+        [HttpPost("market")]
+        public async Task<ActionResult> PostClassified(/*Todo*/)
+        {
+            //ToDo
+        }
+
+        [HttpDelete("market/{id}")]
+        public async Task<IActionResult> BuyClassified(int íd)
+        {
+            //ToDo
+        }
+
+        [HttpDelete("market/{id}")]
+        public async Task<IActionResult> CancelClassified(int íd)
+        {
+            //ToDo
+        }
+
+        [HttpPut("market/quicksell/{productname}/{quantity}")]
+        public async Task<IActionResult> QuicksellProduct(string productname, int quantity)
+        {
+            //ToDo
+        }
+        //ToDo szűréses lekérdezés
+        //ToDo törlés, ha lejár
+    }
+}
