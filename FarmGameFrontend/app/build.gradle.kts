a9c14e0f65582714aa1ca8beb2f77de40a899871
Patrik Csikós <patrik.csikos@proton.me>
2024-11-22 00:37:18 +0100

    min sdk to 26

--- FarmGameFrontend/app/build.gradle.kts
+++ FarmGameFrontend/app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "hu.bme.aut.szoftarch.farmgame"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
