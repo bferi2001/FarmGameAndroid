e80f39ba674acbff3cbe07710c3e045a1d203c98
tuskol <tusko.laszloistvan@gmail.com>
2024-11-21 11:58:55 +0100

    Added base viewmodel to market

--- FarmGameFrontend/app/src/main/java/hu/bme/aut/szoftarch/farmgame/data/market/AdItemData.kt
+++ FarmGameFrontend/app/src/main/java/hu/bme/aut/szoftarch/farmgame/data/market/AdItemData.kt
@@ -1,8 +1,8 @@
 package hu.bme.aut.szoftarch.farmgame.data.market
 
-data class SellingItemData(
+data class AdItemData(
     val item: String,
     val price: Int,
-    var count: Int = 0,
-    var sellCount: Int = 0,
+    val count: Int,
+    val seller: String,
 )
