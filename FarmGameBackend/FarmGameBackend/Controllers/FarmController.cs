1b95030c0d3fb66786f2daa09dbf44b244e0c692
bferi2001 <barnafeco@gmail.com>
2024-11-24 18:24:24 +0100

    unused endpoint deleted

--- FarmGameBackend/FarmGameBackend/Controllers/FarmController.cs
+++ FarmGameBackend/FarmGameBackend/Controllers/FarmController.cs
@@ -11,32 +11,6 @@ public class FarmController(FarmApplicationContext context) : ControllerBase
 {
     User _currentUser = context.GetCurrentUser("testemail@gmail.com");
     
-    [HttpGet("interactions/{landPosition:int}")]
-    public async Task<ActionResult> GetInteractions(int landPosition)
-    {
-        var email = _currentUser.Email;
-        User user = context.GetCurrentUser(email!);
-        int xp = user.UserXP;
-        
-        int unlockedLandCount = xp/100 + 1;
-        if (landPosition < 0 || landPosition >= unlockedLandCount)
-        {
-            return BadRequest("Land position is out of range.");
-        }
-        
-        // Check if the land is empty
-        PlantedPlant? plant = await Helper.Helper.GetPlantByPosition(landPosition, email, context);
-        Barn? barn = await Helper.Helper.GetBarnByPosition(landPosition, email, context);
-
-        if (plant == null && barn == null)
-        {
-            List<string> interactions = ["Build", "Plough"]; // TODO: store in database
-            return Ok(interactions);
-        }
-        
-        return Ok();
-    }
-    
     [HttpGet("size")]
     public ActionResult GetFarmSize()
     {
