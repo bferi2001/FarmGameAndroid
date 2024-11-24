5426d73457ae089c5c992e78a62d154786fc85d8
bferi2001 <barnafeco@gmail.com>
2024-11-24 14:56:23 +0100

    return currentUser

---
+++ FarmGameBackend/FarmGameBackend/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using FarmGameBackend.DbContexts;
+using FarmGameBackend.Entity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FarmGameBackend.Controllers
+{
+    [Route("api")]
+    public class UserController : Controller
+    {
+        private readonly FarmApplicationContext _context;
+        private readonly User _currentUser;
+        public UserController(FarmApplicationContext context)
+        {
+            _context = context;
+            //string? userEmail = HttpContext.Items["Email"]?.ToString();
+            //_currentUser = _context.GetCurrentUser(userEmail!);
+            _currentUser = _context.GetCurrentUser("testemail@gmail.com");
+        }
+        [HttpGet("currentUser")]
+        public async Task<ActionResult<User>> GetCurrentUserAsync()
+        {
+            return _currentUser;
+        }
+    }
+}
